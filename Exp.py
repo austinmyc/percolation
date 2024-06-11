d49b2711398ff97a7aef11ac3ec8d8cab006c7a6
Austin <59735570+austinmyc@users.noreply.github.com>
2024-06-11 17:05:14 +0800

    Add files via upload

---
+++ Exp.py
@@ -0,0 +1,17 @@
+from percolation import *
+from tqdm import tqdm
+
+class Exp:
+    def __init__(self, n, trials):
+        self.n=n
+        self.trials=trials
+        self.res=[-1]*trials
+    
+    def run(self):
+        for r in tqdm(range(self.trials)):
+            P=Percolation_exp(self.n)
+            self.res[r]=P.exp()
+        
+        
+
+
